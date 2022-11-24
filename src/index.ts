412f97a0ab47711da088b35dd61e54861babfea8
Ricki Hastings <rhastings@ebay.com>
2022-11-24 10:53:43 +0000

    docs: update project setup, added TS and Jest to start

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Welcome to your KO Tech Test. Your interviewer will have picked an exercise
+ * and talked you through the process.
+ *
+ * The project has came pre-installed with TypeScript and Jest.
+ *
+ * Run the command `npm run start` to run the project. And `npm test` to run jest.
+ */
+
+console.log('Welcome to your KO Tech Test');
